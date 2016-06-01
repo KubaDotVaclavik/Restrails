476f9404297e3f8eb5e87ff336fe7d95c6c2ac85
Jakub Vaclavik <kuba.vaclavik@outlook.com>
2016-06-01 14:26:07 +0200

    Running devstack

--- Server/app/const.js
+++ Server/app/const.js
@@ -1,3 +1,4 @@
 export const PORT = process.env.PORT || 8081;
 
-export const BUNDLE_URL = 'http://localhost:8080/assets/bundle.js';
+// export const BUNDLE_URL = 'http://localhost:8080/assets/bundle.js';
+export const BUNDLE_URL = 'http://localhost:8080/build/app.js';
