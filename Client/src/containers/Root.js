4c9b40e84eb65a0a4fb47c480085d34db9366314
Jakub Vaclavik <kuba.vaclavik@outlook.com>
2016-06-03 16:58:18 +0200

    HMR

--- Client/src/containers/Root.js
+++ Client/src/containers/Root.js
@@ -4,13 +4,21 @@ import configureStore from '../configureStore'
 import AsyncApp from './AsyncApp'
 
 const store = configureStore()
+let App = AsyncApp
 
 export default class Root extends Component {
     render(){
         return (
             <Provider store={store}>
-                <AsyncApp />
+                <App />
             </Provider>
         )
     }
+}
+
+if (module.hot) {
+    module.hot.accept('./AsyncApp', () => {
+        App = require('./AsyncApp').default; // eslint-disable-line
+        Root.forceUpdate()
+    });
 }
--- Client/src/index.js
+++ Client/src/index.js
@@ -2,9 +2,29 @@ import 'babel-polyfill'
 
 import React from 'react'
 import {render} from 'react-dom'
-import Root from './containers/Root'
+// import Root from './containers/Root'
+import {Provider} from 'react-redux'
+import configureStore from './configureStore' 
+import AsyncApp from './containers/AsyncApp'
+
+const store = configureStore()
+const appRoot = document.getElementById('app-root')
 
 render(
-    <Root/>, 
-    document.getElementById('app-root')
+  <Provider store={store}>
+    <AsyncApp/>
+  </Provider>,
+  appRoot
 )
+
+if (module.hot) {
+    module.hot.accept('./containers/AsyncApp', () => {
+        let NewApp = require('./containers/AsyncApp').default; // eslint-disable-line
+        render(
+            <Provider store={store}>
+                <NewApp/>
+            </Provider>,
+            appRoot
+        )
+    });
+}
